0e0d9a7401f78dc0eeb1183bc19b320814aae503
vt-corey <corey@vtprodesign.com>
2026-02-27 17:18:56 -0800

    Update control.cpp

--- examples/control.cpp
+++ examples/control.cpp
@@ -3,12 +3,11 @@
 #include "SparkFlex.hpp"
 
 /*
-This has been tested with the SPARK MAX while connected to an AndyMark 775 RedLine Motor and
-with a Spark Flex connected to a NEO Vortex Brushless Motor.
+This has been tested with the SPARK MAX while connected to an AndyMark 775
+RedLine Motor and with a Spark Flex connected to a NEO Vortex Brushless Motor.
 */
 
-int main()
-{
+int main() {
   try {
     // Initialize SparkFlex object with CAN interface and CAN ID
     SparkFlex motor("can0", 1);
@@ -28,15 +27,14 @@ int main()
     // Loop for 10 seconds
     auto start = std::chrono::high_resolution_clock::now();
     while (std::chrono::duration_cast<std::chrono::seconds>(
-        std::chrono::high_resolution_clock::now() -
-        start)
-      .count() < 10)
-    {
+               std::chrono::high_resolution_clock::now() - start)
+               .count() < 30) {
       // Enable and run motor
       motor.Heartbeat();
-      motor.SetDutyCycle(0.1);
+      motor.SetDutyCycle(-0.2);
+      std::this_thread::sleep_for(std::chrono::milliseconds(20));
     }
-  } catch (const std::exception & e) {
+  } catch (const std::exception &e) {
     std::cerr << "Error: " << e.what() << std::endl;
     return -1;
   }
